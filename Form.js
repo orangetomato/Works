d8e4cd2891b651a7769f04b778c44981df5e9a6f
orangetomato <itaintme@mail.ru>
2020-01-26 02:17:26 +0300

    Циклы do-while, функция для ФИО, проверка тернарным оператором, переменные для дат

--- Form.js
+++ Form.js
@@ -1,50 +1,48 @@
 'use strict';
 
-let lastName = prompt('Введите вашу фамилию', 'Иванов');
-while (!lastName || lastName.match(/[0-9]/)) {
-  alert('Ошибка');
-  lastName = prompt('Введите вашу фамилию', 'Иванов');
-}
-let firstName = prompt('Введите ваше имя', 'Иван');
-while (!firstName || firstName.match(/[0-9]/)) {
-  alert('Ошибка');
-  firstName = prompt('Введите ваше имя', 'Иван');
-}
-let patronymicName = prompt('Введите ваше отчество', 'Иванович');
-while (!patronymicName || patronymicName.match(/[0-9]/)) {
-  alert('Ошибка');
-  patronymicName = prompt('Введите ваше отчество', 'Иванович');
+function createName(request, defaultValue) {
+    let nameValue;
+    do {
+        nameValue = prompt(request, defaultValue);
+      } while (!nameValue || nameValue.match(/[0-9]/));
+    return nameValue;
 }
 
-let birthYear = Number( prompt('Введите год вашего рождения', '2000') );
+let lastName = createName('Введите вашу фамилию', 'Иванов');
+let firstName = createName('Введите ваше имя', 'Иван');
+let patronymicName = createName('Введите ваше отчество', 'Иванович');
+
 let now = new Date();
-while (birthYear > now.getFullYear() || birthYear < 1900) {
-  alert('Ошибка');
+let currentYear = now.getFullYear();
+let currentMonth = now.getMonth() + 1;
+let currentDay = now.getDate();
+
+let birthYear;
+do {
   birthYear = Number( prompt('Введите год вашего рождения', '2000') );
-}
-let birthMonth = Number( prompt('Введите месяц вашего рождения', '1') );
-while (birthMonth > 12 || birthMonth < 1 
-  || birthYear === now.getFullYear() && birthMonth > now.getMonth() + 1) {
-  alert('Ошибка');
+} while (birthYear > currentYear || birthYear < 1900);
+
+let birthMonth;
+do {
   birthMonth = Number( prompt('Введите месяц вашего рождения', '1') );
-}
-let birthDay = Number( prompt('Введите день вашего рождения', '1') );
-while (birthDay > 31 || birthDay < 1 
-  || birthYear === now.getFullYear() && birthMonth === now.getMonth() + 1 && birthDay > now.getDate()) {
-  alert('Ошибка');
+} while (birthMonth > 12 || birthMonth < 1 
+  || birthYear === currentYear && birthMonth > currentMonth);
+
+let birthDay;
+do {
   birthDay = Number( prompt('Введите день вашего рождения', '1') );
-}
+} while (birthDay > 31 || birthDay < 1 
+  || birthYear === currentYear && birthMonth === currentMonth && birthDay > currentDay);
 
-let birthDate = new Date(birthYear, birthMonth - 1, birthDay);
-let years = now.getFullYear() - birthDate.getFullYear();
-let months = now.getMonth() - birthDate.getMonth();
+let years = currentYear - birthYear;
+let months = currentMonth - birthMonth;
 
-if (now.getMonth() < birthDate.getMonth()) {
+if (currentMonth < birthMonth) {
   years -= 1;
   months += 12;
 }
 
-if (now.getDate() < birthDate.getDate()) {
+if (currentDay < birthDay) {
   months -= 1;
   if (months < 0) {
   years -= 1;
@@ -52,39 +50,24 @@ if (now.getDate() < birthDate.getDate()) {
   }
 }
 
-let gender = prompt('Укажите ваш пол', 'М или Ж');
-while (gender !=='М' && gender !=='Ж') {
-  alert('Ошибка');
+let gender;
+do {
   gender = prompt('Укажите ваш пол', 'М или Ж');
-}
+} while (gender !=='М' && gender !=='Ж');
 
 let kids = confirm('У вас есть дети?');
-
 let kidsNumber;
 if (kids) {
-  kidsNumber = Number( prompt('Сколько у вас детей?', 'Введите число') );
+  do {
+    kidsNumber = Number( prompt('Сколько у вас детей?', 'Введите число') );
+  } while (!kidsNumber);
 } else {
   kidsNumber = 0;
 }
-while (isNaN(kidsNumber)) {
-  alert('Ошибка');
-  kidsNumber = Number( prompt('Сколько у вас детей?', 'Введите число') );
-}
 
 let whoYouAre;
-if (years >= 18) {
-  if (gender === 'М') {
-    whoYouAre = 'совершеннолетний';
-  } else {
-    whoYouAre = 'совершеннолетняя';
-  }
-} else {
-  if (gender === 'М') {
-    whoYouAre = 'несовершеннолетний';
-  } else {
-  whoYouAre = 'несовершеннолетняя';
-  }
-}
+(years >= 18) ? (gender === 'М') ? (whoYouAre = 'совершеннолетний') : (whoYouAre = 'совершеннолетняя') 
+: (gender === 'М') ? (whoYouAre = 'несовершеннолетний') : (whoYouAre = 'несовершеннолетняя');
 
 alert(`
   ${lastName} ${firstName} ${patronymicName}, 
