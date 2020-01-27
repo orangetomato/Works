d77b10e1822fcf86e6ec6ce54aa63b7676f20299
orangetomato <itaintme@mail.ru>
2020-01-27 14:59:26 +0300

    Убрал лишнюю переменную

--- Max number.js
+++ Max number.js
@@ -3,9 +3,8 @@
 function findMaxNumber(arr) {
     let maxNumber = arr[0];
     for (let i = 1; i < arr.length; i++) {
-        let currentNumber = arr[i];
-        if (currentNumber > maxNumber) {
-            maxNumber = currentNumber;
+        if (arr[i] > maxNumber) {
+            maxNumber = arr[i];
         }
     }
     return maxNumber;
@@ -19,9 +18,8 @@ console.log( findMaxNumber([-1, -2, -3]) );
 function findMaxNumber2(...args) {
     let maxNumber2 = args[0];
     for (let i = 1; i < args.length; i++) {
-        let currentNumber2 = args[i];
-        if (currentNumber2 > maxNumber2) {
-            maxNumber2 = currentNumber2;
+        if (args[i] > maxNumber2) {
+            maxNumber2 = args[i];
         }
     }
     return maxNumber2;
