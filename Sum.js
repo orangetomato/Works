5fbdeaf41b09058b9aa37b5c03a6e3a2cbf01ea8
orangetomato <itaintme@mail.ru>
2020-01-29 18:21:25 +0300

    Подправил вывод суммы

--- Sum.js
+++ Sum.js
@@ -1,16 +1,12 @@
 'use strict';
 
-let numbersArray = [ 5, 7,
-  [ 4, [2], 8, [1,3], 2 ],
-  [ 9, [] ],
-  1, 8
-]
+let numbersArray = [ 5, 7, [ 4, [2], 8, [1, 3], 2 ], [ 9, [] ], 1, 8 ];
 
-let sum = 0;
 function getSum(arr) {
+  let sum = 0;
   for (let i = 0; i < arr.length; i++) {
     if ( Array.isArray(arr[i]) ) {
-      getSum(arr[i]);
+      sum += getSum(arr[i]);
     } else {
       sum += arr[i];
     }
