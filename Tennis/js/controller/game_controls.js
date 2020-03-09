d4c566f4b1aeddfa0dd427e688173d458c31b4a2
orangetomato <itaintme@mail.ru>
2020-03-09 16:36:44 +0300

    feat: add keyup event listener

--- Tennis/js/controller/game_controls.js
+++ Tennis/js/controller/game_controls.js
@@ -1,11 +1,13 @@
 export default class GameControls {
     constructor(model) {
-        this.model = model;
+        this.model = model;  
+        this.addListeners();
     }
 
     addListeners() {
         const startButton = document.querySelector('.start-button');
         startButton.addEventListener('click', this.model.start.bind(this.model));
         document.addEventListener('keydown', this.model.keydownHandler.bind(this.model));
+        document.addEventListener('keyup', this.model.keyupHandler.bind(this.model));
     }
 }
