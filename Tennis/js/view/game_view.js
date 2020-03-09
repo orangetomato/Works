1c6ab6aed7b2cb9119b0d5e6fc826d60c60617ee
orangetomato <itaintme@mail.ru>
2020-03-09 16:37:07 +0300

    refactor: relocate objects and functions into model

--- Tennis/js/view/game_view.js
+++ Tennis/js/view/game_view.js
@@ -1,51 +1,11 @@
 export default class GameView {
-    constructor(model, canvasWidth, canvasHeight) {
-        this.model = model;
-
-        this._field = {
-            width: canvasWidth,
-            height: canvasHeight,
-            xPos: 0,
-            yPos: 0,
-            color: 'yellow'
-        }
-
-        this._ball = {
-            radius: 10,
-            xPos: this._field.width / 2,
-            yPos: this._field.height / 2,
-            color: 'red',
-            xSpeed: 3,
-            ySpeed: 1
-        }
-
-        this._leftBat = {
-            width: 10,
-            height: 100,
-            xPos: 0,
-            yPos: 0,
-            color: 'green',
-            ySpeed: 10,
-            score: 0
-        }
-
-        this._rightBat = {
-            width: 10,
-            height: 100,    
-            xPos: this._field.width - 10,
-            yPos: this._field.height - 100,
-            color: 'blue',
-            ySpeed: 10,
-            score: 0
-        }
-
+    constructor(canvasWidth, canvasHeight) {
         const mainWrapper = document.querySelector('.main-wrapper');
         const canvas = this.createCanvasNode(canvasWidth, canvasHeight);
-        const scoreDisplay = this.createScoreDisplay(this._leftBat, this._rightBat);
+        const scoreDisplay = this.createScoreDisplay();
         const startButton = this.createButton();
         mainWrapper.append(startButton, scoreDisplay, canvas);
         this._ctx = canvas.getContext('2d');
-        this.update();
     }
 
     createCanvasNode(canvasWidth, canvasHeight) {
@@ -56,10 +16,9 @@ export default class GameView {
         return canvas;
     }
 
-    createScoreDisplay(leftBat, rightBat) {
+    createScoreDisplay() {
         const scoreDisplay = document.createElement('p');
         scoreDisplay.classList.add('score-display');
-        scoreDisplay.textContent = `${leftBat.score} - ${rightBat.score}`;
         return scoreDisplay;
     }
 
@@ -82,80 +41,16 @@ export default class GameView {
         ctx.fill();
     }
 
-    update() {
-        this._ctx.clearRect(0, 0, this._field.width, this._field.height);
-        this.drawRect(this._field.xPos, this._field.yPos, this._field.width, this._field.height, this._field.color, this._ctx);
-        this.checkBallPosition(this._ball, this._field, this._leftBat, this._rightBat);
-        this.drawCircle(this._ball.xPos, this._ball.yPos, this._ball.radius, this._ball.color, this._ctx);
-        this.drawRect(this._leftBat.xPos, this._leftBat.yPos, this._leftBat.width, this._leftBat.height, this._leftBat.color, this._ctx);
-        this.drawRect(this._rightBat.xPos, this._rightBat.yPos, this._rightBat.width, this._rightBat.height, this._rightBat.color, this._ctx);
-    }
-
-    updateBallPosition(ball) {
-        ball.xPos += ball.xSpeed;
-        ball.yPos += ball.ySpeed;
-        this.update();
-    }
-    
-    checkBallPosition(ball, field, leftBat, rightBat) {
-        if (ball.yPos - ball.radius < field.yPos) {
-            ball.ySpeed = -ball.ySpeed;
-        }
-
-        if (ball.yPos + ball.radius > field.height) {
-            ball.ySpeed = -ball.ySpeed;
-        }
-
-        if (ball.xPos - ball.radius < leftBat.xPos + leftBat.width &&
-            ball.yPos + ball.radius > leftBat.yPos &&
-            ball.yPos - ball.radius < leftBat.yPos + leftBat.height) {
-            ball.xSpeed = -ball.xSpeed;
-        } else if (ball.xPos - ball.radius < field.xPos) {
-            ball.xPos = field.xPos + ball.radius;
-            this.model.stop();
-            rightBat.score++;
-            this.updateScore(leftBat, rightBat);
-        }
-
-        if (ball.xPos + ball.radius > rightBat.xPos &&
-            ball.yPos + ball.radius > rightBat.yPos &&
-            ball.yPos - ball.radius < rightBat.yPos + rightBat.height) {
-            ball.xSpeed = -ball.xSpeed;
-        } else if (ball.xPos + ball.radius > field.width) {
-            ball.xPos = field.width - ball.radius;
-            this.model.stop();
-            leftBat.score++;
-            this.updateScore(leftBat, rightBat);
-        }
+    update(field, ball, leftBat, rightBat) {
+        this._ctx.clearRect(0, 0, field.width, field.height);
+        this.drawRect(field.xPos, field.yPos, field.width, field.height, field.color, this._ctx);
+        this.drawCircle(ball.xPos, ball.yPos, ball.radius, ball.color, this._ctx);
+        this.drawRect(leftBat.xPos, leftBat.yPos, leftBat.width, leftBat.height, leftBat.color, this._ctx);
+        this.drawRect(rightBat.xPos, rightBat.yPos, rightBat.width, rightBat.height, rightBat.color, this._ctx);
     }
 
     updateScore(leftBat, rightBat) {
         const scoreDisplay = document.querySelector('.score-display');
         scoreDisplay.textContent = `${leftBat.score} - ${rightBat.score}`;
-    }
-
-    moveUp(bat, field) {
-        bat.yPos -= bat.ySpeed;
-
-        if (bat.yPos < field.yPos) {
-            bat.yPos = field.yPos;
-        }
-
-        this.update();
-    }
-
-    moveDown(bat, field) {
-        bat.yPos += bat.ySpeed;
-
-        if (bat.yPos + bat.height > field.height) {
-            bat.yPos = field.height - bat.height;
-        }
-
-        this.update();
-    }
-
-    reset(ball, field) {
-        ball.xPos = field.width / 2;
-        ball.yPos = field.height / 2;
     }
 }
