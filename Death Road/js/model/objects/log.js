e2e03551f36666827b21a08d3a4f729fc9676c6a
orangetomato <itaintme@mail.ru>
2020-03-26 14:29:07 +0300

    delete code

--- Death Road/js/model/objects/log.js
+++
@@ -1,8 +0,0 @@
-import Car from './car.js';
-
-export default class Log extends Car {
-    constructor(xPos, yPos, width, height, color, speed) {
-        super(xPos, yPos, width, height, color, speed);
-        this.name = 'Log';
-    }
-}
--- Death Road/js/model/objects/river.js
+++
@@ -1,8 +0,0 @@
-import Rectangle from './rectangle.js';
-
-export default class River extends Rectangle {
-    constructor(xPos, yPos, width, height, color) {
-        super(xPos, yPos, width, height, color);
-        this.name = 'River';
-    }
-}
